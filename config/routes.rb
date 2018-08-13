df7d0ca78e36557e2ec9c6140e39b2875a87e72f
Inou Ridder <inouridder@gmail.com>
2018-08-13 16:12:11 +0200

    navbar implemented messaging

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :spaces do
     resources :favorites, only: [:create]
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create, :new]
     collection do
       post 'query', to: 'spaces#index'
     end
