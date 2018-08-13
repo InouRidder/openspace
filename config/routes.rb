0d84f0cf3003b2a055669700c618074977f9956a
Irvin Hoogland <irvinhoogland@gmail.com>
2018-08-13 17:21:46 +0200

    add i18n

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,19 @@ Rails.application.routes.draw do
   devise_for :users
   mount ActionCable.server => "/cable"
 
-  root to: 'pages#home'
 
-  get ':reviewable/:reviewable_id/reviews/new', to: 'reviews#new', as: :new_review
-  post ':reviewable/:reviewable_id/reviews', to: 'reviews#create', as: :reviews
+  scope '(:locale)', locale: /fr|es/ do
+    root to: 'pages#home'
 
-  resources :spaces do
-    resources :favorites, only: [:create]
-    resources :bookings, only: [:create]
-    collection do
-      post 'query', to: 'spaces#index'
+    get ':reviewable/:reviewable_id/reviews/new', to: 'reviews#new', as: :new_review
+    post ':reviewable/:reviewable_id/reviews', to: 'reviews#create', as: :reviews
+
+    resources :spaces do
+      resources :favorites, only: [:create]
+      resources :bookings, only: [:create]
+      collection do
+        post 'query', to: 'spaces#index'
+      end
     end
   end
 
